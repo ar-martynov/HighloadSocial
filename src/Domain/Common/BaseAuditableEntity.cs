996683c648d92b5bd702a9846f31e5779fe72928
Artem M <sir.aspex@gmail.com>
2023-01-18 17:55:53 +0300

    Basic DDD things added

---
+++ src/Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,12 @@
+namespace Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTime Created { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? UpdatedBy { get; set; }
+}
---
+++ src/Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,6 @@
+namespace Infrastructure.Services;
+
+public class DateTimeService
+{
+    public DateTime Now => DateTime.Now;
+}
